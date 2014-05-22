ba261bc72590edc2f64a43029e30a03b8e88dd0f
xhray <rayfuture@gmail.com>
2014-05-22 15:10:23 +0800

    summary hit stat and show chart on html page

--- iislogs/views.py
+++ iislogs/views.py
@@ -1,10 +1,17 @@
+# -*- coding: UTF-8 -*-
+
 from django.shortcuts import render_to_response, render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from models import iis_logs, hit_stats
 from forms import HitStatQueryForm
 from datetime import datetime
 
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import numpy as np
+import cStringIO
 
 def list(request):
     total = iis_logs.objects().count()
@@ -17,10 +24,23 @@ def listhitstats(request):
     if request.method == 'POST':
         form = HitStatQueryForm(request.POST)
         if form.is_valid():
+            record_limit = 10
             date = form.cleaned_data['day']
-            entities = hit_stats.objects().filter(
-                id__year=date.year, id__month=str(date.month).zfill(2), id__day=str(date.day).zfill(2)).order_by('-value__count')
-            return render(request, 'listhitstats.html', {'entities': entities, 'form': form})
+            entities = hit_stats.objects().filter(id__year=date.year, id__month=str(date.month).zfill(2), id__day=str(date.day).zfill(2)).order_by('-value__count').limit(record_limit)
+
+            fig = Figure(figsize=(15, 25))
+            ax = fig.add_subplot(111)
+            ax.bar(np.arange(len(entities)), map(lambda x : x.value['count'], entities), alpha=0.4)
+            ax.set_xticks(np.arange(record_limit))
+            ax.set_xticklabels(map(lambda x: x.id['url'], entities), rotation=60)
+            ax.set_title('sfa hit stats : {0}'.format(date))
+            canvas = FigureCanvas(fig)
+
+            buf = cStringIO.StringIO()
+            canvas.print_png(buf, format='png')
+            chart = buf.getvalue().encode('base64').strip()
+
+            return render(request, 'listhitstats.html', {'entities':entities, 'form':form, 'chart':chart})
     else:
         form = HitStatQueryForm()
 
--- py_sfa/urls.py
+++ py_sfa/urls.py
@@ -6,13 +6,12 @@ admin.autodiscover()
 from iislogs import views
 
 urlpatterns = patterns('',
-                       # Examples:
-                       # url(r'^$', 'py_sfa.views.home', name='home'),
-                       # url(r'^blog/', include('blog.urls')),
+	   # Examples:
+	   # url(r'^$', 'py_sfa.views.home', name='home'),
+	   # url(r'^blog/', include('blog.urls')),
 
-                       #url(r'^admin/', include(admin.site.urls)),
+	   #url(r'^admin/', include(admin.site.urls)),
 
-                      (r'^iislogs/list$', 'iislogs.views.list'),
-                      (r'^iislogs/listhitstats$',
-                       'iislogs.views.listhitstats'),
-                       )
+	  (r'^iislogs/list$', 'iislogs.views.list'),
+	  (r'^iislogs/listhitstats$', 'iislogs.views.listhitstats'),
+)
